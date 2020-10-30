298cbacddf079c222f579ce5be0866eee44c4d86
pixel-sky <67983872+pixel-sky@users.noreply.github.com>
2020-10-30 12:37:26 -0400

    some minor changes
    added more comment and change .equal() -> .equalsIgnoreCase()

--- 02-temperature_converter/Temperature.java
+++ 02-temperature_converter/Temperature.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class temperature {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        // ask user for which unit to convert to
         System.out.print("to which unit (celcus/fahrenheit): ");
         String unit = input.nextLine();
-        if (unit.equals("celcus")) {
+        
+        // if the user input celcus then covnert to celcus
+        if (unit.equalsIgnoreCase("celcus")) {
             System.out.print("degree: ");
             double temp = input.nextInt();
             System.out.printf("%.1f", fTOc(temp));
         }
+        // else conver to fahrenheit
         else {
             System.out.print("degree: ");
             double temp = input.nextInt();
